0fa44ac18e0859dc504c8a633f94615db947771e
Samaresh Sridhar <ssamaresh1990@gmail.com>
2014-11-24 15:12:32 -0500

    Changes to Loginpage

--- src/app/login/loginCtrl.js
+++ src/app/login/loginCtrl.js
@@ -1,3 +1,4 @@
+
 /* global angular */
 /* global console */
 (function() {
@@ -5,10 +6,28 @@
 		
 	angular
 		.module('loginCtrl', [])
-		.controller('loginCtrl', ['$scope', '$state', function ($scope, $state) {
+		.controller('loginCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
 		$scope.heading = "Log In"; 
 		console.log($scope.heading);
-	}]);
+			
+		// GET LIST OF ALL USERS	
+		$http.get('/users').
+			success(function(data, status, headers, config) {
+				$scope.users = data;	
+				$scope.myUser = $scope.users[0].name;
+			}).
+			error(function(data, status, headers, config) {
+				console.log("NO DATA", status);
+			});
+		
+		//GO TO HOMEPAGE on LOGIN
+			
+		$scope.login = function () {
+			$state.go('home');
+		};
+
+			
+		}]);
 	
 })();
 		
